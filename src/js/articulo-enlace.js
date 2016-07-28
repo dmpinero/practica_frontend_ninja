f5f3b7a6b9bb9c9cfa9897e459dc63aa5d250533
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-07-28 18:19:46 +0200

    Finalizado Me gusta y arreglos en la visualización de elementos

--- src/js/articulo-enlace.js
+++ src/js/articulo-enlace.js
@@ -14,47 +14,36 @@ $(document).ready(function() {
 		var meGustaElemento = getMegusta(id_articulo);	
 		$('#' + id_articulo).text(meGustaElemento);
 	}
-   //var meGustaElemento = getMegusta('articulo1-me-gusta');
-   //$('#articulo1-me-gusta').text(meGustaElemento);
  });
 
 $('body').click(function(event) {
-	console.log("Pulsado en " + event.target.id);
-	//console.log("Más info:" + $(event.target));
-	var total_me_gusta = Number(localStorage.getItem(event.target.id)) + 1;
-	console.log("total_me_gusta para elemento " + event.target.id + " es " + total_me_gusta);
-	
-	$('#' + event.target.id).text(total_me_gusta); // Recargar div del elemento
-	//$('#articulo1-me-gusta').text(total_me_gusta); // Esto funciona
+	//console.log("Pulsado en " + event.target.id);
 
-	localStorage.setItem(event.target.id, total_me_gusta);
-});
-
-// Pulsando en "Me gusta" se aumenta el número 
-/*
-$('.articulo1-me-gusta').on("click", function(event) {
-	console.log("El artículo "+ event.target.id + " me gusta");
+	var meGusta_patron = "-me-gusta";
+	var expreg = new RegExp(meGusta_patron);
 	
-	var meGustaElemento1 = getMegusta(null);
-	if (meGustaElemento1 != -1) // En lavegador soporta Web Storage
+	var encajaMegusta = expreg.test(event.target.id);
+	//console.log("Encaja:" + encaja);
+
+	if (encajaMegusta)
 	{
-		meGustaElemento1 = meGustaElemento1 + 1;
-		localStorage.setItem("articulo1-me-gusta", meGustaElemento1);
-		$('#articulo1-me-gusta').text(meGustaElemento1);
-	}
+		var total_me_gusta = Number(localStorage.getItem(event.target.id)) + 1;
+		//console.log("total_me_gusta para elemento " + event.target.id + " es " + total_me_gusta);
 		
+		$('#' + event.target.id).text(total_me_gusta); // Recargar div del elemento
+		localStorage.setItem(event.target.id, total_me_gusta); // Actualizar Web Storage
+	}
 });
-*/
 
 // Función que recupera el número de "Me gusta" de un elemento
 function getMegusta(elemento)
 {
 	if (typeof(Storage) !== "undefined") { // El navegador soporta Web Storage
-		console.log("Este navegador soporta Web Storage");
+		//console.log("Este navegador soporta Web Storage");
 
 		// Obtener los "Me gusta" almacenados para el artículo
 		var total_me_gusta = Number(localStorage.getItem(elemento));
-		console.log("Este artículo " + elemento + " tiene " + total_me_gusta + " me gusta");
+		//console.log("Este artículo " + elemento + " tiene " + total_me_gusta + " me gusta");
 
 		return total_me_gusta;
 	}	
